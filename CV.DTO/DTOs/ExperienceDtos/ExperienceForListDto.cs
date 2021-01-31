a6dc6bed98b2c85285dc5bb68df31191580c1ec8
Yavuz SAV <yavuzsav07@gmail.com>
2021-01-31 09:04:09 +0300

    Created experience dtos

---
+++ CV.DTO/DTOs/ExperienceDtos/ExperienceForListDto.cs
@@ -0,0 +1,15 @@
+using System;
+using CV.DTO.Interfaces;
+
+namespace CV.DTO.DTOs.ExperienceDtos
+{
+    public class ExperienceForListDto : IDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
