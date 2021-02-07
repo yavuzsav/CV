a1fcfe20c1311fe6a286bfb6addd367786437e0e
Yavuz SAV <yavuzsav07@gmail.com>
2021-02-07 17:59:28 +0300

    Started to integrate admin theme

---
+++ CV.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
